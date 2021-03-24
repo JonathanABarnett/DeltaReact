ce3582a8e3c04661d853e7d7f1277e884b9c975e
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2021-03-24 14:32:00 -0500

    Add Axios get Attendees and fail

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 
 function App() {
-  return <div className="app"></div>;
+  const [attendees, setAttendees] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/attendees").then((response) => {
+      console.log(response);
+      setAttendees(response.data);
+    });
+  }, []);
+
+  return (
+    <div className="app">
+      <ul>
+        {attendees.map((attendee: any) => (
+          <li key={attendee.id}>
+            {attendee.FirstName} {attendee.LastName}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
