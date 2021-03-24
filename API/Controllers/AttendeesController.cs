14dc82b4e01f23b3e4eb4b3050a993b4787eeed6
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2021-03-24 15:23:18 -0500

    Add MediatR to project and refactor controller

--- API/Controllers/AttendeesController.cs
+++ API/Controllers/AttendeesController.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Application.Attendees;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -10,23 +12,26 @@ namespace API.Controllers
 {
     public class AttendeesController : BaseApiController
     {
-        private readonly DataContext _context;
-        public AttendeesController(DataContext context)
+        private readonly IMediator _mediator;
+
+        public AttendeesController(IMediator mediator)
         {
-            _context = context;
+            _mediator = mediator;
+
         }
 
         // /activities
         [HttpGet]
         public async Task<ActionResult<List<Attendee>>> GetAttendees()
         {
-            return await _context.Attendees.ToListAsync();
+            return await _mediator.Send(new List.Query());
         }
 
         // /activities/{id}
-        [HttpGet("{id}")] 
-        public async Task<ActionResult<Attendee>> GetAttendee(Guid id) {
-            return await _context.Attendees.FindAsync(id);
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Attendee>> GetAttendee(Guid id)
+        {
+            return Ok();
         }
     }
 }
