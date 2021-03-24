e93ef576f170f6464795e7abbe5d75cd36ef13bc
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2021-03-24 16:13:03 -0500

    Refactor Mediator to BaseApiController

--- API/Controllers/AttendeesController.cs
+++ API/Controllers/AttendeesController.cs
@@ -3,28 +3,18 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Attendees;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
     public class AttendeesController : BaseApiController
     {
-        private readonly IMediator _mediator;
-
-        public AttendeesController(IMediator mediator)
-        {
-            _mediator = mediator;
-
-        }
 
         // /activities
         [HttpGet]
         public async Task<ActionResult<List<Attendee>>> GetAttendees()
         {
-            return await _mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query());
         }
 
         // /activities/{id}
