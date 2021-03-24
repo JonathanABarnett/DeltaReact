1aa4edacf2c02631782f4cb488bc22f58f652a70
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2021-03-24 11:06:59 -0500

    Clean up base files

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,10 +30,11 @@ namespace API
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "React Delta API", Version = "v1" });
             });
         }
 
+        // Use this for Middleware
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -44,7 +45,7 @@ namespace API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
